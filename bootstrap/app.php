df93219cc52abc8a993379fa01cb0fa6d466859f
Faiz Ilyasa <faizilyasa16@gmail.com>
2025-08-08 15:18:03 +0700

    pembaruan seo, tampilan acara dan artikel, jika tidak ada tampilan maka menggunakan forelse, membuat kesan dan pesan di gabung jadi satu

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use App\Http\Middleware\IsLoggedIn;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsBPH;
+use App\Http\Middleware\IsAnggota;
+use App\Http\Middleware\IsRSDM;
+use App\Http\Middleware\IsAnggotaBPH;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,12 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'isLoggedIn' => \App\Http\Middleware\IsLoggedIn::class,
-            'isAdmin' => \App\Http\Middleware\IsAdmin::class,
-            'isBPH' => \App\Http\Middleware\IsBPH::class,
-            'isAnggota' => \App\Http\Middleware\IsAnggota::class,
-            'isRSDM' => \App\Http\Middleware\IsRSDM::class,
-            'isAnggotaBPH' => \App\Http\Middleware\IsAnggotaBPH::class,
+            'isLoggedIn' => IsLoggedIn::class,
+            'isAdmin' => IsAdmin::class,
+            'isBPH' => IsBPH::class,
+            'isAnggota' => IsAnggota::class,
+            'isRSDM' => IsRSDM::class,
+            'isAnggotaBPH' => IsAnggotaBPH::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
