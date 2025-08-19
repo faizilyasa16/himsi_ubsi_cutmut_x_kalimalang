08026ad697167b450184ebf3f467e0dbce3daff4
Faiz Ilyasa <faizilyasa16@gmail.com>
2025-08-19 10:21:50 +0700

    pembaruan untuk hak akses di beranda

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -10,6 +10,7 @@ use App\Models\Album;
 use App\Models\KesanPesan;
 use App\Models\KontenAlbum;
 use App\Models\JoinHimsi;
+use Illuminate\Support\Facades\Auth;
 
 class BerandaController extends Controller
 {
@@ -18,12 +19,20 @@ class BerandaController extends Controller
     {
         $join = JoinHimsi::first();
         $galeri = KontenAlbum::inRandomOrder()->take(3)->get();
-        $acara = Acara::latest()->take(3)->get();
+        if (Auth::check()) {
+            // Jika user login, tampilkan semua acara
+            $acara = Acara::whereIn('status', ['open', 'closed'])->latest()->paginate(6);
+        } else {
+            // Jika belum login, hanya tampilkan acara dengan nama kategori yang tidak mengandung 'himsi'
+            $acara = Acara::whereHas('kategori', function ($query) {
+                $query->where('nama', 'NOT LIKE', '%himsi%');
+            })->whereIn('status', ['open', 'closed'])->latest()->paginate(6);
+        }
         $anggota = User::whereIn('role', ['anggota', 'bph', 'alumni'])->count();
         $users = User::whereIn('role', ['anggota', 'bph'])->count();
 
-        // Ambil artikel yang statusnya open
-        $artikels = Artikel::whereIn('status', ['open','closed'])->latest()->take(4)->get();
+        // Ambil artikel yang statusnya published
+        $artikels = Artikel::whereIn('status', ['published'])->latest()->take(4)->get();
         $kesanPesan = KesanPesan::inRandomOrder()->whereIn('status', ['active'])->take(6)->get();
         return view('home.beranda.home', compact('acara', 'anggota', 'users', 'artikels', 'galeri', 'kesanPesan', 'join'));
     }
